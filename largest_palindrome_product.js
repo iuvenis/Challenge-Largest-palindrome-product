75ebe931fb7e0192a85fccae1f60c96ac940fa2f
tpyoung <tyleryoung@Tylers-MacBook-Pro.local>
2016-03-15 10:17:35 -1000

    finished challenge

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -10,13 +10,42 @@ module.exports = function(digits){
   var factor_1 = 0;
   var palindromeNumber = 0;
 
-  // do your work here
+function palindromeCheck (){
 
 
+  function PalCheck(factor_0) {
+    factor_0 = '' + factor_0;
+    return factor_0 === factor_0.split("").reverse().join("");
+}
 
+    for ( factor_0 = 99; factor_0 > 1; factor_0--) {
+      for (factor_1 = factor_0; factor_1 > 1; factor_1--) {
+          var productCheck = factor_1 * factor_0;
+          if (PalCheck(productCheck) && productCheck > palindromeNumber) {
+              palindromeNumber = factor_0 * factor_1;
+            }
+        }
+    }
+}
   return {
     factor_0 : factor_0,
     factor_1 : factor_1,
     palindromeNumber : palindromeNumber
   };
 };
+
+function largestPalindrome() {
+    for ( factor_0 = 999; factor_0 > 100; factor_0--) {
+      for (factor_1 = factor_0; factor_1 > 100; factor_1--) {
+          var productCheck = factor_1 * factor_0;
+          if (PalCheck(productCheck) && productCheck > palindromeNumber) {
+              palindromeNumber = factor_0 * factor_1;
+            }
+        }
+    }
+    return palindromeNumber;
+}
+function PalCheck(factor_0) {
+    factor_0 = '' + factor_0;
+    return factor_0 === factor_0.split("").reverse().join("");
+}
